4c2d56f399a02dd49fc9d34f660535186eb262e4
Melissa <melissa_mcguirl@brown.edu>
2019-10-15 11:41:36 -0500

    removed old data

--- src/matlab/my_dist_pfefnac.m
+++
@@ -1,5 +0,0 @@
-function D = my_dist_pfefnac(x, y)
-%boundaryX = 4850; %3710 for WT, 4850 for shady, 4090 for nacre and pfef
-boundaryX = 4090;
-D = (sqrt((x(:,2)-y(:,2)).^2 + min((x(:,1)-y(:,1)).^2, ( boundaryX -abs(x(:,1)-y(:,1))).^2)));
-end 
--- src/matlab/my_dist_shady.m
+++
@@ -1,5 +0,0 @@
-function D = my_dist_shady(x, y)
-%boundaryX = 4850; %3710 for WT, 4850 for shady, 4090 for nacre and pfef
-boundaryX = 4850;
-D = (sqrt((x(:,2)-y(:,2)).^2 + min((x(:,1)-y(:,1)).^2, ( boundaryX -abs(x(:,1)-y(:,1))).^2)));
-end 
